b2f760a7f60f826472205228e37e836784e16113
lmagreda-sanchez <lmagreda-sanchez@myseneca.ca>
2023-09-28 12:27:52 -0400

    Final website

---
+++ Portfolio/src/script.js
@@ -0,0 +1,17 @@
+// global
+
+console.log('Hello World!');
+
+$(function () {
+    'use strict';
+    var view = $(window).height();
+    $('.part')
+        .height(view)
+        .scrollie({
+            scrollOffset: -50,
+            scrollingInView: function (elem) {
+                var bgColor = elem.data('background');
+                $('body').css('background-color', bgColor);
+            }
+        });
+});
